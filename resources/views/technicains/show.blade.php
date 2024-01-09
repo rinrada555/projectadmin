e71a7dbfe54880e539a109ffb0ed13242479390b
mindfeb. <123175947+rinrada555@users.noreply.github.com>
2024-01-10 04:14:43 +0700

    a

--- resources/views/technicains/show.blade.php
+++ resources/views/technicains/show.blade.php
@@ -4,15 +4,12 @@
   
 @section('contents')
 
-
-    <div id="layoutSidenav">
+<div id="layoutSidenav">
     <div id="layoutSidenav_content">
         <main>
-        <h1 class="mb-0">Detail Product</h1>
+        <div class="container-fluid">
+    <h1 class="mb-0">Edit Product</h1>
     <hr />
-    <form action="{{ route('technicains.update', $technicain->id) }}" method="POST">
-        @csrf
-        @method('PUT')
         <div class="row">
             <div class="col mb-3">
                 <label class="form-label">ชื่อ</label>
@@ -33,30 +30,18 @@
                 <input class="form-control" name="Tech_Tel" placeholder="Descriptoin" value="{{ $technicain->Tech_Tel }}">
             </div>
         </div>
-        <div class="row mb-3">
-    <div class="col">
-        <label class="form-label">แผนก</label>
-        <select name="DepartmentID" class="form-select" aria-label="แผนก">
-            <option selected>เลือกแผนก...</option>
-            @foreach($departments as $department)
-                <option value="{{ $department->id }}" @if($department->id == $technicain->DepartmentID) selected @endif>{{ $department->DepartmentType }}</option>
-            @endforeach
-        </select>
-    </div>
-</div>
         <div class="row">
             <div class="d-grid">
-                <button class="btn btn-primary">Update</button>
+                <button class="btn btn-primary">ตกลง</button>
             </div>
         </div>
-    </form>
     <div class="row mt-4">
         <div class="col mb-3">
             <label class="form-label">วันที่เพิ่ม</label>
             <input type="text" name="created_at" class="form-control" placeholder="Created At" value="{{ $technicain->created_at }}" readonly>
         </div>
         <div class="col mb-3">
-            <label class="form-label">แก้ไขล่าสุด</label>
+            <label class="form-label">Updated At</label>
             <input type="text" name="updated_at" class="form-control" placeholder="Updated At" value="{{ $technicain->updated_at }}" readonly>
         </div>
     </div>
